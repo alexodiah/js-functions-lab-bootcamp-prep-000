15139de419a17d0d8161a2cd03691b847efd2e12
Emeka, Alex Odiah <alexodiah@gmail.com>
2019-04-04 05:58:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,15 +9,26 @@ return("Happy holidays!")
 //Write a function named happyHolidaysTo. 
 //This function should accept a parameter of the name of the person you want to wish happy holidays, 
 //and return the string `Happy holidays, ${name}!`
+<<<<<<< HEAD
 var name = "you",
     holiday = "holiday";
+=======
+
+>>>>>>> 0db38dd8b36cff6c6c50a2976ec37e2827c5d8b5
     
 function happyHolidaysTo(name) {
   return(`Happy holidays, ${name}!`)
 }
+<<<<<<< HEAD
 function happyHolidayTo(holiday, name) {
 return(`Happy ${holiday}, ${name}!`)
 }
 function holidayCountdown(holiday, days){
   return(`It's ${days} days until Mother's Day!`)
 }
+=======
+function happyHolidaysTo(holiday, name) {
+return(`"Happy ${holiday}, ${name}!"`)
+}
+
+>>>>>>> 0db38dd8b36cff6c6c50a2976ec37e2827c5d8b5
